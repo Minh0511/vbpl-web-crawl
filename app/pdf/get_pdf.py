4a55c1228f1888eab5645ca15da57fd4a612ac12
Phạm Vũ Minh <minh.pv@teko.vn>
2023-09-11 22:11:07 +0700

    raise exception

--- app/pdf/get_pdf.py
+++ app/pdf/get_pdf.py
@@ -5,28 +5,35 @@ import requests
 
 
 def get_pdf(pdf_url, folder_path):
-    # get vbpl pdf
-    os.makedirs(folder_path, exist_ok=True)
+    try:
+        # get vbpl pdf
+        os.makedirs(folder_path, exist_ok=True)
 
-    file_name_from_url = os.path.basename(pdf_url)
-    decoded_file_name = urllib.parse.unquote_plus(file_name_from_url)
+        file_name_from_url = os.path.basename(pdf_url)
+        decoded_file_name = urllib.parse.unquote_plus(file_name_from_url)
 
-    match = re.search(r'/Attachments/(\d+)/', pdf_url)
-    if match:
-        file_id = match.group(1)
-    else:
-        file_id = "noId"
+        match = re.search(r'/Attachments/(\d+)/', pdf_url)
+        if match:
+            file_id = match.group(1)
+        else:
+            file_id = "noId"
 
-    file_name = f"({file_id})-{decoded_file_name}"
-    file_path = os.path.join(folder_path, file_name)
+        file_name = f"({file_id})-{decoded_file_name}"
+        file_path = os.path.join(folder_path, file_name)
 
-    response = requests.get(pdf_url)
-    if response.status_code == 200:
-        with open(file_path, 'wb') as pdf_file:
-            pdf_file.write(response.content)
-    else:
-        print(f"Failed to download PDF")
+        response = requests.get(pdf_url)
+        if response.status_code == 200:
+            with open(file_path, 'wb') as pdf_file:
+                pdf_file.write(response.content)
+        else:
+            raise Exception("Failed to download PDF from url")
 
-    response.close()
+        return file_path
 
-    return file_path
+    except Exception as e:
+        print(f"Error processing URL: {pdf_url}")
+        print(f"Error message: {str(e)}")
+
+
+
+
