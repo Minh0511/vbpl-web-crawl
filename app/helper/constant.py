8a318201fa1cf32cc277ef9f8c2c597f6dd6809e
Phạm Vũ Minh <minh.pv@teko.vn>
2023-09-19 22:32:03 +0700

    Convert to pdf

--- app/helper/constant.py
+++ app/helper/constant.py
@@ -2,3 +2,4 @@ class AnleSectionConst:
     ANLE_CONTEXT = 'Tình huống án lệ:'
     ANLE_SOLUTION = 'Giải pháp pháp lý:'
     ANLE_CONTENT = 'NỘI DUNG ÁN LỆ'
+    APOSE_WATERMARK = 'Created with an evaluation copy of Aspose.Words. To discover the full versions of our APIs please visit: https://products.aspose.com/words/'
