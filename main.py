8e8b1b84e55abfe020a68f0a2b0823cb9edfbb8f
Phạm Vũ Minh <minh.pv@teko.vn>
2023-09-23 23:58:30 +0700

    Update main cli and retry mechanic for call

--- main.py
+++ main.py
@@ -1,40 +1,110 @@
 import asyncio
-import glob
 import os
-from urllib.parse import quote, urlencode
+import sys
+import time
 
-from bs4 import BeautifulSoup, PageElement
-import requests
-import re
-
-from app.helper.db import LocalSession
 from app.helper.enum import VbplType
-from app.model import Vbpl, Anle
+from app.model import Anle, Vbpl
 from app.service.anle import AnleService
-# import os
-
-# from app.pdf.get_pdf import get_pdf
 
 from app.service.vbpl import VbplService
 
 vbpl_service = VbplService()
 anle_service = AnleService()
 
-asyncio.run(vbpl_service.crawl_all_vbpl(VbplType.PHAP_QUY))
 
-# test_vbpl = Vbpl(
-#     id=32801,
-#     title='Luật 31/VBHN-VPQH',
-#     sub_title='hợp nhất Luật Xử lý vi phạm hành chính',
-# )
-#
-# asyncio.run(vbpl_service.crawl_vbpl_phapquy_info(test_vbpl))
-# asyncio.run(vbpl_service.search_concetti(test_vbpl))
-# asyncio.run(vbpl_service.crawl_vbpl_phapquy_fulltext(test_vbpl))
-#
-# asyncio.run(vbpl_service.crawl_vbpl_hopnhat_info(test_vbpl))
-# asyncio.run(vbpl_service.search_concetti(test_vbpl))
-# asyncio.run(vbpl_service.crawl_vbpl_hopnhat_fulltext(test_vbpl))
+# asyncio.run(vbpl_service.crawl_all_vbpl(VbplType.PHAP_QUY))
 
-# print(test_vbpl)
-# asyncio.run(anle_service.crawl_all_anle())
+# Import your functions here
+# Replace 'crawl_function_1', 'crawl_function_2', and 'function_with_parameter' with your actual function names
+
+def crawl_all_vbpl_phap_quy():
+    # Implement your first crawling function here
+    print("Đang cào dữ liệu vbpl - văn bản pháp quy")
+    asyncio.run(vbpl_service.crawl_all_vbpl(VbplType.PHAP_QUY))
+    print("Cào dữ liệu hoàn tất")
+
+
+def crawl_all_vbpl_hop_nhat():
+    # Implement your second crawling function here
+    print("Đang cào dữ liệu vbpl - văn bản hợp nhất")
+    asyncio.run(vbpl_service.crawl_all_vbpl(VbplType.HOP_NHAT))
+    print("Cào dữ liệu hoàn tất")
+
+
+def craw_all_anle():
+    # Implement your third function that requires an ID parameter here
+    print("Đang cào dữ liệu án lệ")
+    asyncio.run(anle_service.crawl_all_anle())
+    print("Cào dữ liệu hoàn tất")
+
+
+def crawl_anle_by_id(id):
+    print(f"Đang cào dữ liệu của án lệ có id: {id}")
+    new_anle = Anle(doc_id=id)
+    asyncio.run(anle_service.crawl_anle_info(new_anle))
+    print("Cào dữ liệu hoàn tất")
+
+
+def crawl_vbpl_by_id(id):
+    print(f"Đang cào dữ liệu của vbpl có id: {id}")
+    new_vbpl = Vbpl(
+        id=id,
+    )
+    asyncio.run(vbpl_service.crawl_vbpl_phapquy_info(new_vbpl))
+    print("Cào dữ liệu hoàn tất")
+
+
+def print_menu():
+    menu = """
+╔══════════════════════════════════════════════════════╗
+║             Cào dữ liệu vbpl - án lệ                 ║
+╟──────────────────────────────────────────────────────╢
+║ Nhập các lựa chọn sau và ấn enter để chạy chương     ║ 
+║ trình cào dữ liệu, ví dụ: nhập 3 và ấn enter sẽ chạy ║
+║ chương trình cào toàn bộ dữ liệu án lê.              ║ 
+║------------------------------------------------------║ 
+║ 1. Cào dữ liệu vbpl - văn bản pháp quy               ║
+║ 2. Cào dữ liệu vbpl - văn bản hợp nhất               ║
+║ 3. Cào toàn bộ dữ liệu án lệ                         ║
+║ 4. Cào văn bản pháp luật bằng ID                     ║
+║ 5. Cào án lệ bằng ID                                 ║
+║ 6. --help                                            ║
+║ 7. Thoát                                             ║
+╚══════════════════════════════════════════════════════╝
+"""
+    print(menu)
+
+
+def main():
+    print_menu()  # Print the menu initially
+    try:
+        while True:
+            choice = input("Nhập lựa chọn: ")
+
+            if choice == "1":
+                crawl_all_vbpl_phap_quy()
+            elif choice == "2":
+                crawl_all_vbpl_hop_nhat()
+            elif choice == "3":
+                craw_all_anle()
+            elif choice == "4":
+                vbpl_id = input("Nhập ID văn bản: ")
+                crawl_vbpl_by_id(vbpl_id)
+            elif choice == "5":
+                anle_id = input("Nhập ID án lệ: ")
+                crawl_anle_by_id(anle_id)
+            elif choice == "7":
+                print("Đang thoát chương trình.")
+                break
+            elif choice == "6" or choice == "--help":
+                print_menu()  # Display the menu again
+            else:
+                print("Yêu cầu không hợp lệ, để biết các câu lệnh cần dùng, nhập 6 hoặc --help.")
+    except KeyboardInterrupt:
+        print("\nForcefully exiting the CLI.")
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
